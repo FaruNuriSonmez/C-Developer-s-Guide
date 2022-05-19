2b75befec55934053fe98c6313b39226717a0527
Faru Nuri Sönmez <info@farunurisonmez.com>
2022-05-19 12:56:44 +0300

    Chapter 01 - A Hello World
    ...
    
    Co-Authored-By: Faru Nuri Sönmez <info@farunurisonmez.com>

---
+++ 01-A_Hello_World_Program/cmake-build-debug/CMakeFiles/01_A_Hello_World_Program.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "01_A_Hello_World_Program.exe"
+  "01_A_Hello_World_Program.exe.manifest"
+  "01_A_Hello_World_Program.lib"
+  "01_A_Hello_World_Program.pdb"
+  "CMakeFiles/01_A_Hello_World_Program.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/01_A_Hello_World_Program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 01-A_Hello_World_Program/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    printf("Hello World!\n");
+    printf("C Made Easy\n");
+    printf("Herbert Schild\n");
+    return 0;
+}
